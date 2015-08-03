9e469d263e3b60ed53bb260c41541d9b62767cd2
Matt Bernhardt <mjbernha@mit.edu>
2015-08-03 15:12:35 -0400

    Initial commit

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+if ( ! function_exists( 'filter_media_comment_status' ) ) {
+  function filter_media_comment_status( $open, $post_id ) {
+		$post = get_post( $post_id );
+		if( $post->post_type == 'attachment' ) {
+			return false;
+		}
+		return $open;
+	}
+	add_filter( 'comments_open', 'filter_media_comment_status', 10 , 2 );
+}
+
+?>
